26df81998de66c35f8d66a13e781dbf4d9b4d480
Tatsuya Yagi <t_yagi@mebius.co.jp>
2021-05-07 18:57:52 +0900

    5/7の実行コード追加

--- public/js/sample.js
+++ public/js/sample.js
@@ -1,5 +1,29 @@
 (function () {
+
+  $$ = function(elm) {
+    return document.getElementById(elm);
+  }
+
   window.onload = function () {
     console.log("onload called");
   };
+
+  const stopPropagationFlg = false;
+  [
+    "js-event-test",
+    "parent-1",
+    "child-1-1",
+    "child-1-2",
+    "parent-2",
+    "child-2-1",
+    "child-2-2",
+  ].forEach(function(val, i) {
+    $$(val).addEventListener('click', function(e) {
+      e.stopPropagation();
+      alert(`${val} clicked`);
+    })
+  });
+
 })();
+
+
